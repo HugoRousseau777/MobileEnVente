6d64caf035ac45c9293d6d8efdd5f5c42590213a
hugo <hugo0703@wanadoo.fr>
2023-12-20 14:32:56 +0100

    hjl

--- front-end/src/components/signUp.js
+++ front-end/src/components/signUp.js
@@ -17,7 +17,7 @@ const SignUp=()=>{
     
     const collectData=async()=> {
         console.warn(name,email,password);
-        let result = await fetch("http://localhost:5000/register", {
+        let result = await fetch("https://uuu-3fwk.onrender.com/register", {
             method:'post',
             body:JSON.stringify({name,email,password, confirmPassword}),
             headers:{
